b6bd4b525eb078be690b907826b3169027f4d21b
CIC <cic.developer.email@gmail.com>
2021-02-11 21:53:17 +0200

    First

--- app/src/main/java/com/example/fatsecretapiimplementation/ui/MainActivity.java
+++ app/src/main/java/com/example/fatsecretapiimplementation/ui/MainActivity.java
@@ -1,8 +1,11 @@
-package com.example.fatsecretapiimplementation;
+package com.example.fatsecretapiimplementation.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+
+import com.example.fatsecretapiimplementation.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
